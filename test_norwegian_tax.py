97ff2778c14f4fe9f22b6932299d1ecb2a85229e
Lars <lars.larsson@gmail.com>
2021-07-15 21:09:47 +0100

    Update test_norwegian_tax.py
    Measure once, cut twice...

--- test_norwegian_tax.py
+++ test_norwegian_tax.py
@@ -9,7 +9,7 @@ def test_all_cases(_class=tax.NorwegianTax, atol=1e-8, rtol=1e-5):
     So this tests the calculated tax vs the tax written to the config (which originally comes from the Norwegian tax authority's homepage)
     """
     obj = _class()
-    obj.tax_ties_with_config(do_all=True, atol=atol, rtol=rtol)
+    return obj.tax_ties_with_config(do_all=True, atol=atol, rtol=rtol)
 
 
 if __name__ == '__main__':
